34b3019d0b52420517ca0cd0b2ca8130a7a07314
NikolayDevyaty <hvoltage9@yandex.ru>
2024-06-19 14:06:16 +0300

    card added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
-import Header from './components/Header.vue';
+import Header from './components/Header.vue'
+import Card from './components/Card.vue'
 </script>
 
 <template>
   <div class="bg-white h-screen w-4/5 m-auto rounded-xl mt-14 shadow-xl">
-    <Header/>
+    <Header />
+    <Card />
     <h1>Blog</h1>
   </div>
 </template>
