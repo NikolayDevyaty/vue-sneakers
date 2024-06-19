98dc3d3e4d3472d3c18bef713e7b531dde651302
NikolayDevyaty <hvoltage9@yandex.ru>
2024-06-19 11:13:01 +0300

    price added

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@ import MyHello from './components/MyHello.vue';
 </script>
 
 <template>
+  <MyHello title="Puma" :price="10000" :isAdded="true">
+    <button>Купить</button>
+  </MyHello>
+  <MyHello />
+  <MyHello />
   <MyHello />
 </template>
 
--- src/components/MyHello.vue
+++ src/components/MyHello.vue
@@ -1,28 +1,34 @@
 <script setup>
-import { ref } from 'vue'
-let count = ref(50)
-
-function increment() {
-  count.value++
-  console.log(count)
-}
-function inputVal(val) {
-  count.value = val.target.value
-  console.log(count)
-}
+defineProps({
+  title: String,
+  price: Number,
+  isAdded: Boolean
+})
 </script>
 
 <template>
-  <h1>
-    {{ count }}
-  </h1>
-  <button @click="increment">+</button>
-  <!-- <input @input="inputVal" key="count" type="number" /> -->
-  <input @keyup.ctrl.enter="increment" key="count" type="number" />
+  <div class="product">
+    <h2>
+      Товар: {{ title }}
+    </h2>
+    <h3>
+      Цена: {{ price }}
+    </h3>
+    <h3>
+      Добавлен: {{ isAdded }}
+    </h3>
+    <slot></slot>
+  </div>
 </template>
 
-<!-- <style scoped>
-h1 {
+<style scoped>
+.product {
+  border: 1px solid blue;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+/* h1 {
   font-weight: 500;
   font-size: 2.6rem;
   position: relative;
@@ -39,9 +45,10 @@ h3 {
 }
 
 @media (min-width: 1024px) {
+
   .greetings h1,
   .greetings h3 {
     text-align: left;
   }
-}
-</style> -->
+} */
+</style>
