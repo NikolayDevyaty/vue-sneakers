9c94f64e7fe067aedf6d4669e46ce3481d77e4cd
NikolayDevyaty <hvoltage9@yandex.ru>
2024-06-21 19:15:47 +0300

    sort been added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <script setup>
+import { onMounted, ref, watch } from 'vue'
+import axios from 'axios'
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
 // import Drawer from './components/Drawer.vue'
+const items = ref([])
+const sortBy = ref('')
+const onChangeSelect = (event) => {
+  sortBy.value = event.target.value
+}
 // const items = [
-//   { "id": 1, "title": "Кроссовки мужские", "price": 9000, "imageUrl": "/sneakers/sneakers-1.jpg" },
-//   { "id": 2, "title": "Кроссовки женские", "price": 9000, "imageUrl": "/sneakers/sneakers-2.jpg" },
-//   { "id": 3, "title": "Кроссовки детские", "price": 9000, "imageUrl": "/sneakers/sneakers-3.jpg" },
+//   { id: 1, title: 'Кроссовки мужские', price: 9000, imageUrl: '/sneakers/sneakers-1.jpg' },
+//   { id: 2, title: 'Кроссовки женские', price: 9000, imageUrl: '/sneakers/sneakers-2.jpg' },
+//   { id: 3, title: 'Кроссовки детские', price: 9000, imageUrl: '/sneakers/sneakers-3.jpg' }
 // ]
-const items = [
-  { id: 1, title: 'Кроссовки мужские', price: 9000, imageUrl: '/sneakers/sneakers-1.jpg' },
-  { id: 2, title: 'Кроссовки женские', price: 9000, imageUrl: '/sneakers/sneakers-2.jpg' },
-  { id: 3, title: 'Кроссовки детские', price: 9000, imageUrl: '/sneakers/sneakers-3.jpg' }
-]
+onMounted(async () => {
+  try {
+    const { data } = await axios.get('https://2155c769805dbec6.mokky.dev/items')
+    items.value = data
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+watch(sortBy, async () => {
+  try {
+    const { data } = await axios.get(
+      'https://2155c769805dbec6.mokky.dev/items?sortBy=' + sortBy.value
+    )
+    items.value = data
+  } catch (error) {
+    console.log(error)
+  }
+})
 </script>
 
 <template>
@@ -22,10 +43,10 @@ const items = [
       <div class="flex justify-between items-center">
         <h2 class="text-3xl font-bold px-8">Все кроссовки</h2>
         <div class="flex gap-4">
-          <select class="py-2 px-3 border rounded-md outline-none" id="">
-            <option value="">По названию</option>
-            <option value="">По цене (дешевле)</option>
-            <option value="">По цене (дороже)</option>
+          <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none" id="">
+            <option value="name">По названию</option>
+            <option value="price">По цене (дешевле)</option>
+            <option value="-price">По цене (дороже)</option>
           </select>
         </div>
 
